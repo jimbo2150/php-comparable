0aa65c3b0aeb2d9ff29283a805b7531a8af17cfb
Jim Robinson <jimbo2150@gmail.com>
2025-02-22 15:19:42 -0500

    - Added additional mixed comparable tests

--- tests/Suite/Comparable/Examples/MixedComparableTest.php
+++ tests/Suite/Comparable/Examples/MixedComparableTest.php
@@ -20,4 +20,26 @@ final class MixedComparableTest extends TestCase
 			'Cobb salad must not be the same as John.'
 		);
 	}
+
+	public function testMixedComparableSimilar()
+	{
+		$cobbSalad = new GreenFood('cobb salad');
+		$john = new GreenFood('chef salad');
+
+		$this->assertFalse(
+			$cobbSalad->compareTo($john),
+			'Cobb salad must not be the same as chef salad.'
+		);
+	}
+
+	public function testMixedComparableSame()
+	{
+		$chef_salad_1 = new GreenFood('chef salad');
+		$chef_salad_2 = new GreenFood('chef salad');
+
+		$this->assertTrue(
+			$chef_salad_1->compareTo($chef_salad_2),
+			'Chef salad must be the same as chef salad.'
+		);
+	}
 }
