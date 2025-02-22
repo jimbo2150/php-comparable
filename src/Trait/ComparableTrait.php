025ff85626c4e0b32d7dc347b182f41f2e334135
Jim Robinson <jimbo2150@gmail.com>
2025-02-22 11:10:13 -0500

    - Removed attribute - Created utility methods for detecting if object has Comparable trait (ComparableTrait)

--- src/Trait/ComparableTrait.php
+++ src/Trait/ComparableTrait.php
@@ -4,24 +4,18 @@ declare(strict_types=1);
 
 namespace Jimbo2150\PhpComparable\Trait;
 
-use Jimbo2150\PhpComparable\Attribute\Comparable;
 use Jimbo2150\PhpComparable\Enum\Operator;
 
-#[Comparable()]
 trait ComparableTrait
 {
-	#[Comparable()]
 	public function compareTo(
 		object $comparable,
 		Operator $operator = Operator::EQUAL,
 	): bool|int {
 		$comparableReflection = new \ReflectionClass($comparable);
-		if (!isset($comparableReflection->getTraits()[ComparableTrait::class])) {
-			$message = 'Argument $comparable must have trait '.self::class;
-			throw new \InvalidArgumentException($message);
-		}
-
+		self::exceptionOnNonComparableTrait($comparableReflection);
 		/** @var ComparableTrait $comparable */
+
 		return $operator->compare(
 			$this->getComparableValue(),
 			$comparableReflection->
@@ -31,4 +25,18 @@ trait ComparableTrait
 	}
 
 	abstract protected function getComparableValue(): mixed;
+
+	final public static function hasComparableTrait(\ReflectionClass $comparableReflection): bool
+	{
+		return isset($comparableReflection->getTraits()[ComparableTrait::class]);
+	}
+
+	final public static function exceptionOnNonComparableTrait(
+		\ReflectionClass $comparableReflection,
+	): void {
+		if (!self::hasComparableTrait($comparableReflection)) {
+			$message = 'Argument $comparable must have trait '.self::class;
+			throw new \InvalidArgumentException($message);
+		}
+	}
 }
